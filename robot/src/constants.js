51f90189609811950bf238c02e17437f510062b0
atapp <sihogg@bu.edu>
2020-02-11 14:34:53 -0700

    fix for #5

--- robot/src/constants.js
+++ robot/src/constants.js
@@ -12,8 +12,6 @@ export const TABLET_BREAKPOINT_MAX = 1240;
 export const MOBILE_BREAKPOINT_MAX = TABLET_BREAKPOINT_MIN - 1;
 export const DESKTOP_BREAKPOINT_MIN = TABLET_BREAKPOINT_MAX + 1;
 
-export const API_PATH = 'https://harold-server.herokuapp.com/chat';
-
 export const BASE_TITLE = 'Harold the Chat Bot';
 
 export const API_PATH = 'https://harold-server.herokuapp.com/chat'
