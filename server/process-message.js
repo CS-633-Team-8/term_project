698a676ebc7b619b62f124fd91c8f61561ef5011
atapp <sjhogg2@gmail.com>
2020-02-18 15:35:56 -0500

    Added news api

--- server/process-message.js
+++ server/process-message.js
@@ -2,6 +2,9 @@
 
 const Dialogflow = require("dialogflow");
 const Pusher = require("pusher");
+const NewsAPI = require('newsapi');
+const dlv = require('dlv');
+const newsapi = new NewsAPI('39347bcc775544f192bcc6b369fe8ec6');
 
 const projectId = "harold-ctogdt";
 const sessionId = "123456";
@@ -24,6 +27,20 @@ const pusher = new Pusher({
 
 const sessionClient = new Dialogflow.SessionsClient(config);
 
+// use data from intent to  fetch news
+const fetchNews = function (intentData) {
+  const category = 'technology';
+  const country = 'us';
+  const q = dlv(intentData, 'keyword.stringValue');
+
+  return newsapi.v2.topHeadlines({
+    category,
+    language: 'en',
+    country,
+    q
+  })
+}
+
 const processMessage = (sessionId, message) => {
   console.log("mprocessMessage called", projectId, sessionId)
 
@@ -43,10 +60,22 @@ const processMessage = (sessionId, message) => {
     .detectIntent(request)
     .then(responses => {
       const result = responses[0].queryResult;
-      return pusher.trigger("bot", "bot-response", {
-        message: result.fulfillmentText,
-        sessionId: sessionId
-      });
+      const intentData = dlv(responses[0], 'queryResult.parameters.fields')
+      if (result && result.intent && result.intent.displayName == "news.search") {
+        fetchNews(intentData)
+          .then(news => news.articles)
+          .then(articles = pusher.trigger('bot', 'bot-response', {
+            news: articles.splice(0, 6),
+            message: result.fulfillmentText,
+            sessionId: sessionId
+          })) 
+      } else {
+        pusher.trigger("bot", "bot-response", {
+          message: result.fulfillmentText,
+          sessionId: sessionId
+        });
+      }
+      return res.sendStatus(200)
     })
     .catch(err => {
       console.error("ERROR:", err);
