bd7282cca5801eb7a2644cbe553c0416f45a5a45
atapp <sjhogg2@gmail.com>
2020-02-23 13:16:25 -0500

    Feature/release2 (#11)
    * Added news api
    
    * Update for news api
    
    * release2
    
    * bug 1 fix
    
    Co-authored-by: cs633team8 <60204565+cs633team8@users.noreply.github.com>

--- robot/src/constants.js
+++ robot/src/constants.js
@@ -1,7 +1,7 @@
-export const GOOGLE_ANALYTICS_ID = 'UA-6032469-95';
-export const LAUNCH_DARKLY_LOCAL_KEY = '5b3aef8602390a14e79e2600';
-export const LAUNCH_DARKLY_STAGING_KEY = '5b3aef818b34c314e516d34a';
-export const LAUNCH_DARKLY_PRODUCTION_KEY = '5b39e1d5cb05a814d7dbce98';
+export const GOOGLE_ANALYTICS_ID = "UA-6032469-95";
+export const LAUNCH_DARKLY_LOCAL_KEY = "5b3aef8602390a14e79e2600";
+export const LAUNCH_DARKLY_STAGING_KEY = "5b3aef818b34c314e516d34a";
+export const LAUNCH_DARKLY_PRODUCTION_KEY = "5b39e1d5cb05a814d7dbce98";
 
 export const MOBILE_NAV_HEIGHT = 54;
 
@@ -12,6 +12,7 @@ export const TABLET_BREAKPOINT_MAX = 1240;
 export const MOBILE_BREAKPOINT_MAX = TABLET_BREAKPOINT_MIN - 1;
 export const DESKTOP_BREAKPOINT_MIN = TABLET_BREAKPOINT_MAX + 1;
 
-export const BASE_TITLE = 'Harold the Chat Bot';
+export const BASE_TITLE = "Harold the Chat Bot";
 
-export const API_PATH = 'https://harold-server.herokuapp.com/'
+export const API_PATH = "https://harold-server.herokuapp.com";
+
