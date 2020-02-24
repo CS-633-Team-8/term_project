3eb7e0cbd34556d8180bf821b4a146381926763a
atapp <sihogg@bu.edu>
2020-02-23 20:35:53 -0500

    login redirect

--- robot/src/components/CatchNetlify.js
+++ robot/src/components/CatchNetlify.js
@@ -4,6 +4,7 @@ import Form, { Field, FormFooter } from '@atlaskit/form';
 import TextField from '@atlaskit/textfield';
 import Button from '@atlaskit/button';
 import { useIdentityContext } from 'react-netlify-identity';
+import { useHistory } from 'react-router-dom'
 
 export default function CatchNetlifyRecoveryNullComponent() {
   const formRef = React.useRef()
@@ -22,7 +23,11 @@ export default function CatchNetlifyRecoveryNullComponent() {
               console.log(e)
               const email = e.email
               const password = e.password
-              signupUser(email, password)
+              signupUser(email, password).then(user => {
+                console.log('Success! Signed up', user);
+                useHistory.push('/faqs');
+              })
+              .catch(err => console.error(err));
             }}>
             {({ formProps }) => (
               <form {...formProps}>
