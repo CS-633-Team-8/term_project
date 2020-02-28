3ee82595f6fcd4f81a6d308505704694bea5ccd6
atapp <sjhogg2@gmail.com>
2020-02-28 14:24:30 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,27 +1,47 @@
 // server.js
+/** 
+  * Main routing file for Harold Chatbot server
+  * Routes:
+  * .get(/faqs)
+  * .post(/faqs)
+  * .post(/chat)
+*/
 
+////////////////////////////////////////////////////////////////////////////
+// DEPENDENCIES
 require('dotenv').config({ path: 'variables.env' });
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const processMessage = require('./process-message');
 const processFAQS = require('./process-faqs');
 
+////////////////////////////////////////////////////////////////////////////
+// CLASS INSTANCES
 const app = express();
 
+////////////////////////////////////////////////////////////////////////////
+// CONFIGURATION
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+////////////////////////////////////////////////////////////////////////////
+// ROUTES
 
+/** 
+  * get FAQs
+  * Express route which returns JSON payload of Knowledge Base FAQs
+  * Secured by secret ID
+*/
 
 app.get('/faqs', (req, res) => {
 
+  // Retrieve secret header from request
   const secretHeader = req.header('secretID');
-  //console.log(secretHeader);
   
-  if (secretHeader === 'ilovecs633') {
+  // Check if valid, if not return 401
+  if (secretHeader === process.env.OUR_LITTLE_SECRET) {
     processFAQS.getFAQSAsJSON().then(json => 
       res.json(json)
       );
@@ -32,9 +52,17 @@ app.get('/faqs', (req, res) => {
   }
 });
 
+/** 
+  * edit FAQs
+  * Express route which receives JSON payload of new KnowledgeBase FAQs
+  * Secured by secret ID
+*/
+
 app.post('/faqs', (req, res) => {
+  
+  // Retrieve secret header from request
   const secretHeader = req.header('secretID');
-  //console.log(secretHeader);
+  
   if (secretHeader === process.env.OUR_LITTLE_SECRET) {
     processFAQS.replaceFAQs(req.body)
   } else {
@@ -42,6 +70,12 @@ app.post('/faqs', (req, res) => {
     res.statusCode = 401;
   }
 });
+
+/** 
+  * post chat
+  * Express route which allows for message posting to server
+  * Secured by secret ID
+*/
 
 app.post('/chat', (req, res) => {
   const { message, sessionId } = req.body;
